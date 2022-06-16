f44f971888841cb9335105ac102c31d5b495636b
Panoskontos <panos96el@gmail.com>
2022-06-16 22:20:51 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Simple component
+// Tecnhically Babel creates the html elements while using 
+// as little as possible syntax
+
+// vars
+let name = "Panos";
+let lname = "Kontos"
+let age = 1996;
+
 const App = () => {
-    return (
-    <div>
-        <h1>Hello</h1>
-    </div>
-    )
+    return (<h2>Welcome {name + ' ' +lname} {2022-age}</h2>)
 }
 
 // Where to render
