96bb33b9839de0785ea5fac2ffc995d261d323d8
Panoskontos <panos96el@gmail.com>
2022-06-17 00:06:45 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -34,13 +34,44 @@ const printAlert =(message) =>{
 }
 
 return (
-    <>  App
+    <>  
 
         {/* passing props */}
         <Heading content="Top Content" number={0} array={['','.','..']} object={{name:"panos",skill:"coding"}}/>
         <Heading content="My books" number={2} 
         // without () so it won't trigger
         printAlert={printAlert}/>
+        <br />
+
+
+        {/* children help when you want to pass diff html in the same comp*/}
+        <Body background="black" width="300px">
+        
+        {/* Children */}
+        <div>Logo</div>
+        <nav>
+            <a href="#">1</a>
+            <br />
+            <a href="#">2</a>
+            <br />
+            <a href="#">3</a>
+        </nav>
+
+        </Body> 
+       
+       
+       
+        <Body background="aqua" width="200px">
+        
+        {/* Children */}
+        <div>Logo</div>
+        <nav>
+            <a href="#">1</a>
+            <br />
+            <a href="#">2</a>
+        </nav>
+
+        </Body> 
 
     </>
 )
@@ -55,8 +86,27 @@ const Heading = (props) => {
         <div>
             <h1>Header - {props.content} - {props.number==0 ? "No number was inputted": props.number}</h1>
             
-            {/* Button with my function */}
-            <button onClick={props.printAlert==undefined ? null :props.printAlert.bind(null,"Hi")}>Press</button>
+            {/* Button with my function 2 ways*/}
+            {/* props */}
+            <button onClick={props.printAlert==undefined ? null :props.printAlert.bind(null,"Hi")}>Press with Props</button>
+            
+            {/* Better - Function */}
+            <button onClick={()=> props.printAlert("Hello")}>Press with function</button>
+        </div>
+    )
+}
+
+
+// Passing props to Children
+const Body = (props) =>{
+    return (
+        <div style={{
+            background:props.background,
+            width:props.width,
+            height:'200px',
+            color:"white",
+        }}>
+            {props.children}
         </div>
     )
 }
