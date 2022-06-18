c2d6f70cdbf7185d7720c37790d3ba5b20d4983f
Panoskontos <panos96el@gmail.com>
2022-06-18 21:42:10 +0300

    components

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,54 @@ import Logo from './Logo';
 import Header from './Header';
 import Card from './Card';
 
+
+let data = 
+    [
+        {
+            fullName: 'Matt Giampietro',
+    status: 'friend',
+    description: '. . . ',
+    totalFriends: 75,
+    joinedYear: 2013,
+        },
+
+        {fullName: 'Riki per sempre',
+        status: 'enemy',
+        description: '. . . ',
+        totalFriends: 26,
+        joinedYear: 2010,}
+
+        ,
+        {fullName: 'Mantolero',
+        status: 'frenemy',
+        description: '. . . ',
+        totalFriends: 7000,
+        joinedYear: 2013,}
+    ]
+
 export default function() {
+
+
+    // long way to loop cards
+    const printCards=()=>{
+        const cards = []
+        for (let i=0;i<data.length;i++){
+            console.log(i)
+            cards.push(<Card UserData={data[i]}/>)
+        }
+
+        return cards
+    }
+
+
 return (
     <>  
     <Logo children="Hello"/>
     <Header content="Panos" number={10}/>
-    <Card 
-    image1="https://i.pinimg.com/originals/97/35/2a/97352a18b151610dc66f3f693e0b9196.png" 
-    image2="https://i.pinimg.com/originals/97/35/2a/97352a18b151610dc66f3f693e0b9196.png"/>
+    <Card image1="" UserData = {data} />
+    
+    {/* loop function */}
+    {printCards()}
     </>
 )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 
+
 export default function(props){
     return (
         
@@ -17,41 +18,20 @@ export default function(props){
       <img src={props.image1} />
     </div>
     <div className="content">
-      <a className="header">Team Fu</a>
+      <a className="header">{props.UserData.fullName}</a>
       <div className="meta">
         <span className="date">Created in Sep 2014</span>
       </div>
     </div>
     <div className="extra content">
       <a>
-        <i className="users icon" />2 Members
+        <i className="users icon" />{props.UserData.totalFriends} Nerds
       </a>
     </div>
   </div>
-  <div className="card">
-    <div className="blurring dimmable image">
-      <div className="ui inverted dimmer">
-        <div className="content">
-          <div className="center">
-            <div className="ui primary button">Add Friend</div>
-          </div>
-        </div>
-      </div>
-      <img src={props.image2} />
-    </div>
-    <div className="content">
-      <a className="header">Team Hess</a>
-      <div className="meta">
-        <span className="date">Created in Aug 2014</span>
-      </div>
-    </div>
-    <div className="extra content">
-      <a>
-        <i className="users icon" />2 Members
-      </a>
-    </div>
-  </div>
+  
 </div>
+
     )
       
 }
