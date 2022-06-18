b710b80186527aa43450f40c5422d381dba95150
Panoskontos <panos96el@gmail.com>
2022-06-18 22:40:48 +0300

    components

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,17 @@ import React from "react";
 
 // a file we can export and reuse
 const Button = (props) => {
+    // let buttonRef = React.createRef();
+    let clickMe = () => {
+        console.log("God Damn");
+        
+    }
+
     return(
         <button
+        // ref={buttonRef}
+        // using DOM in react
+        onClick={clickMe}
         id="button"
         style={{
             color:"white",
@@ -11,7 +20,8 @@ const Button = (props) => {
             border:'None',
             width:"300px",
             height:"60px",
-            borderRadius:"20px"
+            borderRadius:"20px",
+            fontSize:'1.3rem',
         }}
         >
             {props.children}
